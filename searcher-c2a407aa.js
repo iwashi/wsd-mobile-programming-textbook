27fc943684992a0fcc2742fa0e398aee0f440f98
iwashi <iwashi@users.noreply.github.com>
2025-11-23 11:43:37 +0000

    deploy: 9361eb66d9a91ae39170e2b09ded7136ddf40ba1

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-acfe9f5d.js',
+                path_to_root + 'searchindex-d3e6f78f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
